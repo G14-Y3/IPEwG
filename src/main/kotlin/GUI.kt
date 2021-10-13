21c354a6118663bc6d6d9f8158bdb191c1dc4035
shitong xu <tonyshengxue@outlook.com>
2021-10-13 23:23:52 +0100

    sx119: milestone: WIP, implementing zoom in image at coordinate in viewport

--- src/main/kotlin/GUI.kt
+++ src/main/kotlin/GUI.kt
@@ -1,7 +1,7 @@
-import javafx.beans.InvalidationListener
 import javafx.beans.property.DoubleProperty
 import javafx.beans.property.SimpleDoubleProperty
 import javafx.geometry.Pos
+import javafx.geometry.Rectangle2D
 import javafx.scene.control.Alert
 import javafx.scene.control.TextField
 import javafx.scene.image.ImageView
@@ -13,40 +13,78 @@ import tornadofx.*
 
 
 var oriImageView: ImageView by singleAssign()
+// double value that define the image width, changes as zoom event happens
+var zoomedWidth: DoubleProperty = SimpleDoubleProperty(WINDOW_WIDTH)
+// window x and y value, defining the left up corner position of the image
+var windowX: DoubleProperty = SimpleDoubleProperty(0.0)
+var windowY: DoubleProperty = SimpleDoubleProperty(0.0)
+var image_h_w_ratio: Double = 0.0
 
 class GUI : View("IPEwG") {
 
     private val testImageUrl = resources.url("test_image.png").toURI()
 
-    // double value that define the image width, changes as zoom event happens
-    val zoomedWidth: DoubleProperty = SimpleDoubleProperty(MAX_IMAGE_WIDTH)
     override val root = borderpane {
         center {
             vbox {
-                vbox {
-                    scrollpane {
-                        // setup zoomProperty to detect zoom in & out action
-                        this.addEventFilter(ZoomEvent.ANY) {
-                            if (it.zoomFactor > 1) {
-                                zoomedWidth.set(zoomedWidth.get() * 1.05)
-                            } else if (it.zoomFactor < 1) {
-                                // only zoom out when image is smaller than original image
-                                if (zoomedWidth.get() > MAX_IMAGE_WIDTH * 1.05) {
-                                    zoomedWidth.set(zoomedWidth.get() / 1.05)
-                                }
+                // todo: zoom in with the mouse position as the zoom center
+                scrollpane {
+                    // listen to scroll property, change the window x y value
+                    this.addEventFilter(ScrollEvent.ANY) {
+
+                        // calculating the view box's left top coordinate's x, y values' upperbound
+                        var xBound = oriImageView.fitWidth - WINDOW_WIDTH
+                        var yBound = 0.0
+                        if (oriImageView.image.height > WINDOW_HEIGHT) {
+                            yBound = oriImageView.fitHeight - WINDOW_HEIGHT
+                        }
+
+                        windowX.set(cast(0.0, xBound, windowX.get() - it.deltaX))
+//                        windowY.set(cast(0.0, yBound, windowY.get() - it.deltaY))
+
+//                        println("x = " + windowX.get())
+//                        println("y = " + windowY.get())
+
+                    }
+                    // listen to zoomProperty to detect zoom in & out action
+                    this.addEventFilter(ZoomEvent.ANY) {
+                        var ratio = 1.0
+                        if (it.zoomFactor > 1) {
+                            ratio = 1.01
+                        } else if (it.zoomFactor < 1) {
+                            // only zoom out when image is smaller than original image
+                            if (zoomedWidth.get() > WINDOW_WIDTH ) {
+                                ratio = 1 / 1.01
                             }
                         }
-                        zoomedWidth.addListener(InvalidationListener {
-                            oriImageView.fitWidth = zoomedWidth.get()
-                        })
-                        //oriImage
-                        imageview(ImageController().load(testImageUrl).get(raw = true)) {
-                            oriImageView = this
-                            this.preserveRatioProperty().set(true)
-                        }
+                        // update image size and left top coordinate according to the ratio
+                        zoomedWidth.set(zoomedWidth.get() * ratio)
+                        oriImageView.fitWidth = zoomedWidth.get()
+
+                        println("x = " + oriImageView.image.width)
+                        println("y = " + oriImageView.image.height)
+//                        println("scene = " + oriImageView.scaleX)
+//                        windowX.set(windowX.get() * ratio)
+//                        windowY.set(windowY.get() * ratio)
+                        // update view box's left top coordinate
+//                        oriImageView.viewport = Rectangle2D(windowX.get(), windowY.get(), WINDOW_WIDTH, WINDOW_HEIGHT)
                     }
-                    this.maxWidth = MAX_IMAGE_WIDTH
-                    this.maxHeight = MAX_IMAGE_HEIGHT
+                    //oriImage
+                    imageview(ImageController().load(testImageUrl).get(raw = true)) {
+                        oriImageView = this
+                        image_h_w_ratio = oriImageView.y / oriImageView.x
+//                        println(oriImageView.y)
+//                        println(oriImageView.fitHeight)
+//                        println(oriImageView.image.height)
+                        oriImageView.fitWidth = zoomedWidth.get()
+                        oriImageView.preserveRatioProperty().set(true)
+                    }
+                    // fixing the window for displaying image
+                    // todo: any more elegant way of fixing window size?
+                    this.minWidth = WINDOW_WIDTH
+                    this.maxWidth = WINDOW_WIDTH
+                    this.minHeight = WINDOW_HEIGHT
+                    this.maxHeight = WINDOW_HEIGHT
                 }
                 stackpane {
                     alignment = Pos.CENTER
@@ -122,5 +160,7 @@ class GUIController : Controller() {
     fun show(imageUrl: String) {
         //if (imageUrl != "") println(imageUrl)
         oriImageView.image = ImageController().loadByPath(imageUrl).get(raw = true)
+        windowX.set(0.0)
+        windowY.set(0.0)
     }
 }
--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -1,3 +1,13 @@
-// constants for displaying one image
-const val MAX_IMAGE_HEIGHT = 600.0
-const val MAX_IMAGE_WIDTH = 400.0
+// constants for one image's displaying window
+const val WINDOW_W_H_RATIO = 1.0
+const val WINDOW_WIDTH = 400.0
+const val WINDOW_HEIGHT = WINDOW_WIDTH * WINDOW_W_H_RATIO
+
+fun cast(min: Double, max: Double, v: Double): Double {
+    if (v < min) {
+        return min
+    } else if (v > max) {
+        return max
+    }
+    return v
+}
