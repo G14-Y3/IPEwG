536a42366583de313fc03b07097ae2925a50ea92
Shitian Jin <shitian.jin18@imperial.ac.uk>
2021-10-13 12:14:15 +0800

    feat(gui): add buttons to import image from file path

--- src/main/kotlin/GUI.kt
+++ src/main/kotlin/GUI.kt
@@ -1,15 +1,77 @@
+import javafx.geometry.Pos
+import javafx.scene.control.TextField
+import javafx.stage.FileChooser
+import javafx.stage.StageStyle
+import models.*
 import tornadofx.*
 
-class GUI: View() {
+//lateinit var oriImage: Image
+
+class GUI: View("IPEwG") {
+
     private val testImageUrl = resources.url("test_image.png").toURI()
 
     override val root = borderpane {
         center {
-            hbox {
+            vbox {
                 stackpane {
+                    //oriImage
                     imageview(ImageController().load(testImageUrl).get(raw = true))
+                }
+                stackpane {
+                    alignment = Pos.CENTER
+                    button("Open...") {
+                        action {
+                            find(ImportImage::class).openModal(stageStyle = StageStyle.UTILITY)
+                        }
+                    }
                 }
             }
         }
+    }
+}
+
+class ImportImage: Fragment("Import image..") {
+    var imageUrlField: TextField by singleAssign()
+    val controller: GUIController by inject()
+    override val root = borderpane{
+        center {
+            vbox {
+                stackpane {
+                    hbox {
+                        form {
+                            fieldset {
+                                field("Path") {
+                                    textfield() {
+                                        imageUrlField = this
+                                    }
+                                    button("browse..") {
+                                        action {
+                                            val filter = arrayOf(FileChooser.ExtensionFilter("PNG files (*.png)", "*.png"),
+                                                FileChooser.ExtensionFilter("Bitmap files (*.bmp)", "*.bmp"),
+                                                FileChooser.ExtensionFilter("JPEG files (*.jpeg, *.jpg)", "*.jpeg", "*.jpg"))
+                                            var dir = chooseFile("Select image", filters = filter, mode = FileChooserMode.Single)
+                                            imageUrlField.text = if (dir.toString() == "[]")  "" else dir[0].toString()
+                                        }
+                                    }
+                                }
+                            }
+                            alignment = Pos.CENTER
+                            button("OK") {
+                                action {
+                                    controller.output(imageUrlField.text)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+class GUIController: Controller() {
+    fun output(imageUrl: String) {
+        if (imageUrl != "") println(imageUrl)
     }
 }
