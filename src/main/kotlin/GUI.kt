f6d5b96ca5f436ed1313dd6dec4eeac9cdc87ff8
Shitian Jin <shitian.jin18@imperial.ac.uk>
2021-10-13 15:55:22 +0800

    format: reformat the code to google java style

--- src/main/kotlin/GUI.kt
+++ src/main/kotlin/GUI.kt
@@ -8,7 +8,7 @@ import tornadofx.*
 
 var oriImageView: ImageView by singleAssign()
 
-class GUI: View("IPEwG") {
+class GUI : View("IPEwG") {
 
     private val testImageUrl = resources.url("test_image.png").toURI()
 
@@ -34,10 +34,10 @@ class GUI: View("IPEwG") {
     }
 }
 
-class ImportImage: Fragment("Import image..") {
+class ImportImage : Fragment("Import image..") {
     private var imageUrlField: TextField by singleAssign()
     private val controller: GUIController by inject()
-    override val root = borderpane{
+    override val root = borderpane {
         center {
             vbox {
                 stackpane {
@@ -50,11 +50,28 @@ class ImportImage: Fragment("Import image..") {
                                     }
                                     button("browse..") {
                                         action {
-                                            val filter = arrayOf(FileChooser.ExtensionFilter("PNG files (*.png)", "*.png"),
-                                                FileChooser.ExtensionFilter("Bitmap files (*.bmp)", "*.bmp"),
-                                                FileChooser.ExtensionFilter("JPEG files (*.jpeg, *.jpg)", "*.jpeg", "*.jpg"))
-                                            val dir = chooseFile("Select image", filters = filter, mode = FileChooserMode.Single)
-                                            imageUrlField.text = if (dir.toString() == "[]")  "" else dir[0].toString()
+                                            val filter = arrayOf(
+                                                FileChooser.ExtensionFilter(
+                                                    "PNG files (*.png)",
+                                                    "*.png"
+                                                ),
+                                                FileChooser.ExtensionFilter(
+                                                    "Bitmap files (*.bmp)",
+                                                    "*.bmp"
+                                                ),
+                                                FileChooser.ExtensionFilter(
+                                                    "JPEG files (*.jpeg, *.jpg)",
+                                                    "*.jpeg",
+                                                    "*.jpg"
+                                                )
+                                            )
+                                            val dir = chooseFile(
+                                                "Select image",
+                                                filters = filter,
+                                                mode = FileChooserMode.Single
+                                            )
+                                            imageUrlField.text =
+                                                if (dir.toString() == "[]") "" else dir[0].toString()
                                         }
                                     }
                                 }
@@ -70,7 +87,8 @@ class ImportImage: Fragment("Import image..") {
                                             type = Alert.AlertType.ERROR,
                                             header = "Invalid image path",
                                             content = "The image path you entered is incorrect.\n" +
-                                                    "Please check!")
+                                                    "Please check!"
+                                        )
                                     }
                                 }
                             }
@@ -82,7 +100,7 @@ class ImportImage: Fragment("Import image..") {
     }
 }
 
-class GUIController: Controller() {
+class GUIController : Controller() {
     fun show(imageUrl: String) {
         //if (imageUrl != "") println(imageUrl)
         oriImageView.image = ImageController().loadByPath(imageUrl).get(raw = true)
