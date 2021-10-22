46aabc24f6b2be8c269de876516fff11331ac583
shitong xu <tonyshengxue@outlook.com>
2021-10-22 19:19:52 +0100

    sx119: initial class for frequency domain process

--- src/main/kotlin/processing/ImageProcessing.kt
+++ src/main/kotlin/processing/ImageProcessing.kt
@@ -3,6 +3,8 @@ package processing
 import javafx.scene.image.WritableImage
 
 enum class RGBType {R, G, B}
+enum class FreqProcessType {Idle}
+enum class FreqProcessRange {HighPass, LowPass}
 
 interface ImageProcessing {
 
---
+++ src/main/kotlin/processing/frequency_domain_transfer/FrequencyFilters.kt
@@ -0,0 +1,13 @@
+package processing.frequency_domain_transfer
+
+import javafx.scene.image.WritableImage
+import processing.FreqProcessRange
+import processing.FreqProcessType
+import processing.ImageProcessing
+
+class FrequencyFilters(private val type : FreqProcessType, private val range : FreqProcessRange) : ImageProcessing{
+
+    override fun process(image: WritableImage) {
+        TODO("Not yet implemented")
+    }
+}
