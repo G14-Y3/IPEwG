G14-Y3/IPEwG

commit f3b073f83ae7436773fb25fa43ee989728d7be43

refactor(slider-with-spinner): extract out spinner section as `doubleSpinner`

src/main/kotlin/view/component/SliderWithSpinner.kt
@@ -1,6 +1,8 @@
 package view.component
 
+import javafx.beans.property.Property
 import javafx.beans.value.ChangeListener
+import javafx.event.EventTarget
 import javafx.geometry.Insets
 import javafx.scene.Node
 import javafx.scene.control.ComboBox
@@ -11,10 +13,55 @@ import tornadofx.*
 import view.CssStyle
 import kotlin.math.roundToInt
 
+inline fun <reified T : Number> EventTarget.doubleSpinner(
+    min: T? = null,
+    max: T? = null,
+    initialValue: T? = null,
+    amountToStepBy: T? = null,
+    editable: Boolean = false,
+    property: Property<T>? = null,
+    enableScroll: Boolean = false,
+    noinline op: Spinner<T>.() -> Unit = {}
+): Spinner<T> {
+    val spinner = spinner(
+        min = min,
+        max = max,
+        initialValue = initialValue,
+        amountToStepBy = amountToStepBy,
+        editable = editable,
+        property = property,
+        enableScroll = enableScroll,
+        op = op,
+    )
+
+    // avoid NPE and set value to old value when user clear the field
+    spinner.valueProperty()
+        .addListener { _, old, new ->
+            spinner.valueFactory.value = new ?: old
+        }
+
+    // use Regex to make sure user inputs a double not character string
+    spinner.editor.textProperty()
+        .addListener { _, old, new ->
+            try {
+                if (!new.matches(Regex("-?\\d*\\.?\\d*"))) {
+                    spinner.editor.text = old
+                } else {
+                    spinner.editor.text =
+                        new.toDouble().roundToInt().toString()
+                }
+            } catch (e: IllegalArgumentException) {
+            }
+        }
+
+    return spinner
+}
+
 class SliderWithSpinner(
     private val minVal: Double,
     private val maxVal: Double,
-    private val op: ChangeListener<Number>) : HBox() {
+    private val op: ChangeListener<Number>,
+) : HBox() {
 
     private var comboBox: ComboBox<*>? = null
     private lateinit var slider: Slider
@@ -70,26 +117,6 @@ class SliderWithSpinner(
         ) {
             maxWidth = 70.0
         }
-
-        // avoid NPE and set value to old value when user clear the field
-        spinner.valueProperty()
-            .addListener { _, old, new ->
-                spinner.valueFactory.value = new ?: old
-            }
-
-        // use Regex to make sure user inputs a double not character string
-        spinner.editor.textProperty()
-            .addListener { _, old, new ->
-                try {
-                    if (!new.matches(Regex("-?\\d*\\.?\\d*"))) {
-                        spinner.editor.text = old
-                    } else {
-                        spinner.editor.text =
-                            new.toDouble().roundToInt().toString()
-                    }
-                } catch (e: IllegalArgumentException) {
-                }
-            }
 
         try {
             slider.valueProperty().bindBidirectional(

commit d80ed663f87509e838a6291385706dad5ff96eef

fix(slider-with-spinner): nPE: issue introduced in f3b073f83ae7436773fb25fa43ee989728d7be43

src/main/kotlin/view/component/SliderWithSpinner.kt
@@ -108,7 +108,7 @@ class SliderWithSpinner(
         slider.valueProperty().addListener(op)
         this.add(slider)
 
-        spinner = spinner(
+        spinner = doubleSpinner(
             min = minVal,
             max = maxVal,
             initialValue = 0.0,