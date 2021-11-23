6f2216cb2e0889b15d02b6ac423b05487d3b0901
zhaoxuan <zhaoxuan0914@hotmail.com>
2021-11-23 16:49:27 +0000

    xz1919: changed layout for depth estimation tab

--- src/main/kotlin/view/component/DepthEstimationTab.kt
+++ src/main/kotlin/view/component/DepthEstimationTab.kt
@@ -14,73 +14,81 @@ class DepthEstimationTab(engineController: EngineController, engine: EngineModel
     private lateinit var colormap: DepthColorMap
 
     init {
-        vbox {
-            label("Depth Estimation") {
-                vboxConstraints {
-                    margin = Insets(20.0, 20.0, 10.0, 10.0)
-                }
-                style {
-                    fontWeight = FontWeight.BOLD
-                    fontSize = Dimension(20.0, Dimension.LinearUnits.px)
-                }
-            }
-            hbox {
-                vbox {
-                    hbox {
-                        label("Estimating the depth using ") {
-                            hboxConstraints {
-                                marginTop = 5.0
-                                marginBottom = 20.0
-                            }
-                        }
-                        combobox(values = DepthEstimationModel.values().toList()) {
-                            valueProperty()
-                                .addListener(ChangeListener { _, _, new ->
-                                    modelType = new
-                                })
-
-                            value = DepthEstimationModel.NYU
-                        }
-                    }
-                    hbox {
-                        label("Using ") {
-                            hboxConstraints {
-                                marginTop = 5.0
-                                marginBottom = 20.0
-                            }
-                        }
-                        combobox(values = DepthColorMap.values().toList()) {
-                            valueProperty()
-                                .addListener(ChangeListener { _, _, new ->
-                                    colormap = new
-                                })
-
-                            value = DepthColorMap.Viridis
-                        }
-                        label(" as the depth color map") {
-                            hboxConstraints {
-                                marginTop = 5.0
-                                marginBottom = 20.0
-                            }
-                        }
-                    }
-                }
-
-                button("depth") {
-                    action {
-                        engineController.depthEstimation(modelType, colormap)
-                    }
-                    hboxConstraints {
+        hbox {
+            vbox {
+                label("Depth Estimation") {
+                    vboxConstraints {
                         margin = Insets(20.0, 20.0, 10.0, 10.0)
                     }
-                }
-                imageview(engine.depthImage) {
-                    isPreserveRatio = true
-                    fitWidth = 200.0
-                    fitHeight = 200.0
-                    hboxConstraints {
-                        margin = Insets(20.0)
+                    style {
+                        fontWeight = FontWeight.BOLD
+                        fontSize = Dimension(20.0, Dimension.LinearUnits.px)
                     }
+                }
+                hbox {
+                    vbox {
+                        hbox {
+                            vboxConstraints {
+                                margin = Insets(20.0, 20.0, 10.0, 10.0)
+                            }
+                            label("Estimating the depth using ") {
+                                hboxConstraints {
+                                    marginTop = 5.0
+                                    marginBottom = 20.0
+                                }
+                            }
+                            combobox(values = DepthEstimationModel.values().toList()) {
+                                valueProperty()
+                                    .addListener(ChangeListener { _, _, new ->
+                                        modelType = new
+                                    })
+
+                                value = DepthEstimationModel.NYU
+                            }
+                        }
+                        hbox {
+                            vboxConstraints {
+                                margin = Insets(0.0, 20.0, 10.0, 10.0)
+                            }
+                            label("Using ") {
+                                hboxConstraints {
+                                    marginTop = 5.0
+                                    marginBottom = 20.0
+                                }
+                            }
+                            combobox(values = DepthColorMap.values().toList()) {
+                                valueProperty()
+                                    .addListener(ChangeListener { _, _, new ->
+                                        colormap = new
+                                    })
+
+                                value = DepthColorMap.Viridis
+                            }
+                            label(" as the depth color map") {
+                                hboxConstraints {
+                                    marginTop = 5.0
+                                    marginBottom = 20.0
+                                }
+                            }
+                        }
+
+                        button("Estimate Depth") {
+                            action {
+                                engineController.depthEstimation(modelType, colormap)
+                            }
+                            vboxConstraints {
+                                margin = Insets(0.0, 20.0, 10.0, 10.0)
+                            }
+                        }
+                    }
+                }
+            }
+            imageview(engine.depthImage) {
+                isPreserveRatio = true
+                fitWidth = 200.0
+                fitHeight = 200.0
+                hboxConstraints {
+                    margin = Insets(20.0)
                 }
             }
         }
