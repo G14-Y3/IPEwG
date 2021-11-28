6868f14d72ce975a53c5a03249f60647ccc0b37b
Joey Teng <joey.teng.dev@gmail.com>
2021-11-28 23:44:41 +0000

    feat(resample): implement UI support for bicubic params

--- src/main/kotlin/view/component/ResizerTab.kt
+++ src/main/kotlin/view/component/ResizerTab.kt
@@ -1,10 +1,12 @@
 package view.component
 
 import controller.EngineController
-import javafx.beans.property.IntegerProperty
-import javafx.beans.property.SimpleIntegerProperty
+import javafx.beans.property.*
+import javafx.event.EventTarget
 import javafx.geometry.Insets
 import javafx.scene.text.FontWeight
+import processing.resample.BicubicParams
+import processing.resample.Params
 import processing.resample.ResampleMethod
 import tornadofx.*
 
@@ -29,58 +31,85 @@ class ResizerTab : Fragment("Resize Image") {
         }
     )
 
-    override val root = vbox {
-        val model = DimensionModel(ImgSize(previewWidth, previewHeight))
+    override val root = scrollpane(fitToWidth = true, fitToHeight = false) {
+        vbox {
+            val model = DimensionModel(ImgSize(previewWidth, previewHeight))
+            val paramsModel = KernelParamsModel(KernelParams())
 
-        label("Resize Image") {
-            vboxConstraints {
-                margin = Insets(20.0, 20.0, 10.0, 10.0)
+            label("Resize Image") {
+                vboxConstraints {
+                    margin = Insets(20.0, 20.0, 10.0, 10.0)
+                }
+                style {
+                    fontWeight = FontWeight.BOLD
+                    fontSize = Dimension(20.0, Dimension.LinearUnits.px)
+                }
             }
-            style {
-                fontWeight = FontWeight.BOLD
-                fontSize = Dimension(20.0, Dimension.LinearUnits.px)
+
+            padding = Insets(20.0, 20.0, 10.0, 10.0)
+
+            val resizerList = ResampleMethod.values().toList()
+            val comboBox = combobox(values = resizerList) {
+                itemsProperty().onChange { }
+                value = resizerList[0]
             }
-        }
 
-        padding = Insets(20.0, 20.0, 10.0, 10.0)
-
-        val resizerList = ResampleMethod.values().toList()
-        val comboBox = combobox(values = resizerList)
-        comboBox.value = resizerList[0]
-
-        form {
-            fieldset("Dimensions") {
-                field("width") {
-                    textfield(model.width).validator {
-                        if (it.isNullOrBlank()) error("This field is required")
-                        else {
-                            val message =
-                                widthValidator[comboBox.value]?.let { it1 -> it1(it.toInt()) }
-                            if (message != null) error(message) else null
+            form {
+                fieldset("Dimensions") {
+                    field("width") {
+                        textfield(model.width).validator {
+                            if (it.isNullOrBlank()) error("This field is required")
+                            else {
+                                val message =
+                                    widthValidator[comboBox.value]?.let { it1 -> it1(it.toInt()) }
+                                if (message != null) error(message) else null
+                            }
                         }
                     }
-                }
-                field("height") {
-                    textfield(model.height).validator {
-                        if (it.isNullOrBlank()) error("This field is required")
-                        else {
-                            val message =
-                                heightValidator[comboBox.value]?.let { it1 -> it1(it.toInt()) }
-                            if (message != null) error(message) else null
+                    field("height") {
+                        textfield(model.height).validator {
+                            if (it.isNullOrBlank()) error("This field is required")
+                            else {
+                                val message =
+                                    heightValidator[comboBox.value]?.let { it1 -> it1(it.toInt()) }
+                                if (message != null) error(message) else null
+                            }
                         }
                     }
-                }
-                button("Resize") {
-                    enableWhen(model.valid)
-                    action {
-                        engineController.resample(
-                            previewWidth,
-                            previewHeight,
-                            model.width.value,
-                            model.height.value,
-                            comboBox.value,
-                            null,
+                    bicubicParamsField(paramsModel) {
+                        text(
+                            """
+                        B + 2C = 1 is recommended.
+                        Common Choices:
+                        - B: 0, C: Any - Cardinal Splines
+                        - B: 0, C: 0.5 - Catmull-Rom Spline, used in GIMP
+                        - B: 0, C: 0.75 - Unnamed, used in Adobe Photoshop
+                        - B: 1/3, C: 1/3 - Mitchell-Netravali, used in ImageMagick
+                        - B: 1, C: 0 - B-spline, used in Paint.net
+                        """.trimIndent()
                         )
+                        whenVisible {
+                            paramsModel.doubleArg1.value = 1.0 / 3
+                            paramsModel.doubleArg2.value = 1.0 / 3
+                        }
+                        visibleWhen {
+                            comboBox.selectionModel.selectedItemProperty()
+                                .isEqualTo(ResampleMethod.Bicubic)
+                        }
+                        managedProperty().bind(visibleProperty())
+                    }
+                    button("Resize") {
+                        enableWhen(model.valid)
+                        action {
+                            engineController.resample(
+                                previewWidth,
+                                previewHeight,
+                                model.width.value,
+                                model.height.value,
+                                comboBox.value,
+                                paramsModel.getParams(comboBox.value),
+                            )
+                        }
                     }
                 }
             }
@@ -90,13 +119,54 @@ class ResizerTab : Fragment("Resize Image") {
 
 class ImgSize(width: Int, height: Int) {
     val widthProperty = SimpleIntegerProperty(this, "width", width)
-    var name by widthProperty
-
     val heightProperty = SimpleIntegerProperty(this, "height", height)
-    var height by heightProperty
 }
 
 class DimensionModel(dimension: ImgSize) : ItemViewModel<ImgSize>(dimension) {
     val width = bind(ImgSize::widthProperty) as IntegerProperty
     val height = bind(ImgSize::heightProperty) as IntegerProperty
+}
+
+fun EventTarget.bicubicParamsField(
+    model: KernelParamsModel,
+    op: Fieldset.() -> Unit = {},
+): Fieldset =
+    fieldset("Bicubic Kernel Params") {
+        field("Param B (larger blurrier):") {
+            textfield(model.doubleArg1).validator {
+                if (it.isNullOrBlank()) error("This field is required")
+                else if (it.isDouble() && it.toDouble() in .0..1.0) {
+                    null
+                } else {
+                    error("B must be between 0.0 to 1.0 (inclusive)")
+                }
+            }
+        }
+        field("Param C (larger more ringing):") {
+            textfield(model.doubleArg2).validator {
+                if (it.isNullOrBlank()) error("This field is required")
+                else if (it.isDouble() && it.toDouble() in .0..1.0) {
+                    null
+                } else {
+                    error("C must be between 0.0 to 1.0 (inclusive)")
+                }
+            }
+        }
+    }.also(op)
+
+class KernelParams {
+    val doubleArg1Property = SimpleDoubleProperty(this, "doubleArg1", .0)
+    val doubleArg2Property = SimpleDoubleProperty(this, "doubleArg2", .0)
+}
+
+class KernelParamsModel(params: KernelParams) : ItemViewModel<KernelParams>(params) {
+    val doubleArg1 = bind(KernelParams::doubleArg1Property) as DoubleProperty
+    val doubleArg2 = bind(KernelParams::doubleArg2Property) as DoubleProperty
+
+    fun getParams(method: ResampleMethod): Params? = when (method) {
+        ResampleMethod.Point -> null
+        ResampleMethod.PointWithZeros -> null
+        ResampleMethod.Bilinear -> null
+        ResampleMethod.Bicubic -> BicubicParams(doubleArg1.value, doubleArg1.value)
+    }
 }
